2610e3a1b3d79ad5faa12378600a6a8e6e65958d
abiona01 <quadratwemimo@gmail.com>
2022-04-22 21:58:14 +0100

    update

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import axios from "axios";
 import ReactLoading from "react-loading";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-
 import {
   AppChild,
   AppContainer,
